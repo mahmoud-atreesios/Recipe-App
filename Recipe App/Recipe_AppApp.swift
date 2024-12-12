f960977d5480e48f7079015e9f0b96fb624897da
mahmoud-atreesios <136765078+mahmoud-atreesios@users.noreply.github.com>
2024-12-12 20:24:29 +0200

    Basic commit

---
+++ Recipe App/Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_AppApp.swift
+//  Recipe App
+//
+//  Created by Mahmoud Mohamed Atrees on 12/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
